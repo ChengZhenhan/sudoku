89a6fde06b8a248f099cc336b003c44079e4119c
ChengZhenhan <315746595@qq.com>
2022-11-17 17:20:04 +0800

    fix

--- src/draw.cpp
+++ src/draw.cpp
@@ -3,17 +3,14 @@
 #include "draw.h"
 #include "Utils.h"
 
-void draw::draw_board(int nx,int ny)
-{
+void draw::draw_board(int nx,int ny) {
     gotoxy(0, 0);
     print_color(1,"━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
     std::cout << std::endl;
     int color;
-    for(int i = 0;i < 9;i++)
-    {
+    for(int i = 0;i < 9;i++) {
         print_color(1,"┃");
-        for(int j = 0;j < 9;j++)
-        {
+        for(int j = 0;j < 9;j++) {
             color = (j+1)%3==0?1:0;
             std::cout << " ";
             if(i == ny && j == nx)
