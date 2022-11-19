2eb4adedbe1844321ad621e702750e7d9fb02397
ChengZhenhan <315746595@qq.com>
2022-11-19 21:09:44 +0800

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,59 @@
-#include <iostream>
-#include <cstring>
-#include <cmath>
-#include "draw.h"
 #include "CreateSudoku.h"
 #include "Utils.h"
+#include "draw.h"
+#include <cmath>
+#include <cstring>
+#include <iostream>
 
-void play() {
-	cls();
+void play()
+{
+    cls();
 
-	// clock_t startTime,endTime;
-	// startTime = clock();
-	s:
-	std::cout << "Welcome to Sudoku!" << std::endl;
-	std::cout << "Please input the difficulty level you want[1.easy 2.medium 3.difficult]:" << std::endl;
-	int level = 0;
-	std::cin >> level;
-	cls();
-	if(level >= 1 && level <= 3)
-	{
-		CreateSudoku sudoku(20 * level);
-		draw d(sudoku.get_board());
-		d.draw_board(0,0);
-		sudoku.get_input();
-	}
-	else
-	{
-		std::cout << "ERROR:Invalid input!" << std::endl;
-		goto s;
-	}
+// clock_t startTime,endTime;
+// startTime = clock();
+s:
+    std::cout << "Welcome to Sudoku!" << std::endl;
+    std::cout << "Please input the difficulty level you want[1.easy 2.medium 3.difficult]:" << std::endl;
+    int level = 0;
+    std::cin >> level;
+    cls();
+    if (level >= 1 && level <= 3) {
+        CreateSudoku sudoku(20 * level);
+        draw d(sudoku.get_board());
+        d.draw_board(0, 0);
+        sudoku.get_input();
+    } else {
+        std::cout << "ERROR:Invalid input!" << std::endl;
+        goto s;
+    }
 }
-void play(const std::string& filename) {
-	cls();
-	CreateSudoku sudoku(filename);
-	draw d(sudoku.get_board());
-	d.draw_board(0,0);
-	sudoku.get_input();
+void play(const std::string &filename)
+{
+    cls();
+    CreateSudoku sudoku(filename);
+    draw d(sudoku.get_board());
+    d.draw_board(0, 0);
+    sudoku.get_input();
 }
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 #ifdef _WIN32
-	system("chcp 65001");
+    system("chcp 65001");
 #endif
-	m:
-	if(argc == 1) {
-		play();
-	}
-	else if(argc == 2 && !strcmp(argv[1], "-h")) {
-		std::cout << "Usage: ./Sudoku [filename]" << std::endl;
-		std::cout << "If no filename is given, the game will start automatically." << std::endl;
-		return 0;
-	}
-	else if(argc == 2) {
-		play(argv[2]);
-	}
-	else {
-		std::cout << "Usage: ./Sudoku [filename]" << std::endl;
-		std::cout << "If no filename is given, the game will start automatically." << std::endl;
-		goto m;
-	}
-	while(true){}
+m:
+    if (argc == 1) {
+        play();
+    } else if (argc == 2 && !strcmp(argv[1], "-h")) {
+        std::cout << "Usage: ./Sudoku [filename]" << std::endl;
+        std::cout << "If no filename is given, the game will start automatically." << std::endl;
+        return 0;
+    } else if (argc == 2) {
+        play(argv[2]);
+    } else {
+        std::cout << "Usage: ./Sudoku [filename]" << std::endl;
+        std::cout << "If no filename is given, the game will start automatically." << std::endl;
+        goto m;
+    }
+    while (true) { }
     return 0;
 }
